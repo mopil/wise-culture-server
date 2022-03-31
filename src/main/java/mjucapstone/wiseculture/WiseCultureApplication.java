c30d1f95417cdba0a11d01e0bb4b025da0a8387b
Rena <Rena@Rena>
2022-03-31 18:27:09 +0900

    commit test

--- src/main/java/mjucapstone/wiseculture/WiseCultureApplication.java
+++ src/main/java/mjucapstone/wiseculture/WiseCultureApplication.java
@@ -3,6 +3,8 @@ package mjucapstone.wiseculture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// commit test
+
 @SpringBootApplication
 public class WiseCultureApplication {
 
