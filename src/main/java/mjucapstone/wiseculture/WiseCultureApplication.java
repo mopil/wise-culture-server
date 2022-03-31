da486a913592b9b9806b60af1494af1fb5983ac3
mopil1102 <mopil1102@gmail.com>
2022-03-31 11:08:13 +0900

    Initial commit

---
+++ src/main/java/mjucapstone/wiseculture/WiseCultureApplication.java
@@ -0,0 +1,13 @@
+package mjucapstone.wiseculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseCultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseCultureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mjucapstone/wiseculture/WiseCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package mjucapstone.wiseculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseCultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
