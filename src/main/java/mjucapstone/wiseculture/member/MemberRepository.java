a41f51300bf81410e9f817f7837736655520666b
mgc39 <minkc01@mju.ac.kr>
2022-04-10 18:13:17 +0900

    Revert "로그인/로그아웃(세션,쿠키활용)"
    This reverts commit f4b1d9b644e9adc76e818f4c21f3f56c68c18f1b.

--- src/main/java/mjucapstone/wiseculture/member/MemberRepository.java
+++ src/main/java/mjucapstone/wiseculture/member/MemberRepository.java
@@ -1,14 +1,12 @@
 package mjucapstone.wiseculture.member;
 
+import mjucapstone.wiseculture.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import mjucapstone.wiseculture.member.domain.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository <Member, Long>{
 
     boolean existsByName(String name);
     boolean existsByNickname(String nickname);
-    Member findByUserId(String userId);
 }
